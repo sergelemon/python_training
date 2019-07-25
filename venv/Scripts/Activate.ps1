afea2834f7aa0efe8d4c8438aa3aed510012e5e2
Sergey Limanchuk <53022632+sergelemon@users.noreply.github.com>
2019-07-25 10:02:22 +0300

    111

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Sergey\Documents\GitHub\python_training\venv"
+$env:VIRTUAL_ENV="C:\Users\User\Documents\GitHub\python_training\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/easy_install-3.7-script.py
+++ venv/Scripts/easy_install-3.7-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\Sergey\Documents\GitHub\python_training\venv\Scripts\python.exe
+#!C:\Users\User\Documents\GitHub\python_training\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
 __requires__ = 'setuptools==40.8.0'
 import re
